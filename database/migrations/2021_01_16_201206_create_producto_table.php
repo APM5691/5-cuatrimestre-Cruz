940bf5ef98d28088b8f0ba49ef778939168b80e0
DSM52 Angel Palacios Mirafuentes <al221910938@gmail.com>
2021-01-16 14:49:18 -0600

    Migración y modelo Producto

---
+++ database/migrations/2021_01_16_201206_create_producto_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('producto', function (Blueprint $table) {
+            $table->id();
+            $table->char('clave', 25);
+            $table->char('producto', 200);
+            $table->integer('existencias');
+            $table->float('precio_unitario', 10, 2);
+            $table->enum('unidad_medida', ['Pieza', 'Caja', 'Kilogramo', 'Metro', 'Bolsa', 'Otro']);
+            $table->enum('estatus', ['Activo', 'Inactivo']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('producto');
+    }
+}
