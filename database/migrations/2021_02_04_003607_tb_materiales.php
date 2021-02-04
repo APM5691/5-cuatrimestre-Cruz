7c4df17a96a413b145936a9fa383e16e1b035952
Abril Reyes Gonzalez <al221910465@gmail.com>
2021-02-03 19:11:01 -0600

    migraciones productos y materiales

---
+++ database/migrations/2021_02_04_003607_tb_materiales.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class TbMateriales extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('TbMateriales', function (Blueprint $table) {
+            $table->id()->conment('id');
+            $table->string ('tipo_material',200)->conment('Tipo de material');
+            $table->string('material_principal',200)->conment('Material principal');
+            $table->string('productos_clave_producto',200)->conment('Productos clave de producto');
+            $table->timestamps();
+        });
+    }
+    
+    
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_02_04_005642_productos.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Productos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id()->conment('id');
+            $table->string ('clave',25)->conment('Clave');
+            $table->string('nombre_producto',200)->conment('Nombre de producto');
+            $table->unsignedInteger('existencias')->conment('Existencias');
+            $table->decimal('precio',10,2)->conment('Precio ');
+            $table->string('descripcion',200)->conment('Descripcion del producto');
+            $table->string('Medida',200)->conment('Descripcion medida');
+            $table->decimal('precio_oferta',10,2)->conment('Precio oferta');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
