1a80c299a044837cd583004e1da9862e7916056f
DSM52 Angel Palacios Mirafuentes <al221910938@gmail.com>
2021-02-10 21:15:10 -0600

    Solo falta los demas

--- app/Http/Controllers/Api/UsuariosController.php
+++ app/Http/Controllers/Api/UsuariosController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Resources\UserResource;
 use App\Http\Resources\UserCollection;
 use App\Models\User;
 
@@ -17,7 +18,6 @@ class UsuariosController extends Controller
     public function index()
     {
         return new UserCollection(User::all());
-
     }
 
     /**
@@ -38,8 +38,14 @@ class UsuariosController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required|string|max:25', 'primer_apellido' => 'required|string|max:25', 'segundo_apellido' => 'bail|nullable|string|max:25', 'fecha_nacimiento' => 'required|date_format:Y-m-d', 'sexo' => 'required|in:Femenino,Masculino,Prefiere no decirlo', 'perfil' => 'required|in:Administrador,Operador,Mecánico', 'estatus' => 'required|in:Activo,Inactivo', 'email' => 'required|email|unique:users,email', 'password' => 'required|min:8'
+        ]);
+        $request->merge(['password' => bcrypt($request->password)]);
+        $usuario = User::create($request->all());
+        return new UserResource($usuario);
     }
+
 
     /**
      * Display the specified resource.
